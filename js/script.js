c4d19a72122ccd4ea99b08ddddb4ae56aee569dc
roberto.boston <roberto.bozzo97@tiscali.it>
2023-01-09 21:07:13 +0100

    fix code

--- js/script.js
+++ js/script.js
@@ -10,9 +10,14 @@ createApp({
     },
     methods: {
         generateMail (){
+            if(this.mailArray.length == 10){
+                this.mailArray = []
+            }
+
             for(let i = 0; i<10; i++){
                 
                 axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) => {
+                    console.log(result)
                     this.mailArray.push(result.data.response)
                 })
 
