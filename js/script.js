fc6a93617f8965e6c1c99ea19a0d953f9c953902
roberto.boston <roberto.bozzo97@tiscali.it>
2023-01-09 20:49:16 +0100

    excercise

--- js/script.js
+++ js/script.js
@@ -10,11 +10,12 @@ createApp({
     },
     methods: {
         generateMail (){
-            for(let i = 0; i<=10; i++){
+            for(let i = 0; i<10; i++){
+                
                 axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) => {
                     this.mailArray.push(result.data.response)
-                    console.log(mailArray)
                 })
+
             }
         }
     },
